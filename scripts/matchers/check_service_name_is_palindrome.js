3835897cc04f069db48ed305c156c33ba369a0a9
Andrew Steinborn <git@steinborn.me>
2018-02-17 18:57:43 -0500

    Remove this, it's on the wiki now.

--- scripts/matchers/check_service_name_is_palindrome.js
+++
@@ -1,9 +0,0 @@
-function matchVote(vote) {
-    return palindrome(vote.getServiceName());
-}
-
-// This is a very rough way to check if a word is a palindrome.
-function palindrome(word) {
-    var cleaned = word.toLowerCase().replace(/[^a-z0-9]/g, "");
-    return cleaned.split("").reverse().join("") == cleaned;
-}
